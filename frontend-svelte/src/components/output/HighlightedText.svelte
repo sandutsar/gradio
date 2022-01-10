d2b8a3b7863d28e230de89433beb4fcc62d48d18
dawoodkhan82 <dawoodkhan82@gmail.com>
2022-01-10 18:08:40 -0500

    label output

---
+++ frontend-svelte/src/components/output/HighlightedText.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let value, theme;
+    export let color_map;
+  </script>
+  
+  <div
+    {theme}
+  >
+    {value}
+  </div>
+  
+
+  <style lang="postcss">
+  </style>
--- frontend-svelte/src/components/output/Label.svelte
+++ frontend-svelte/src/components/output/Label.svelte
@@ -1,51 +1,52 @@
 <script>
-    export let value, setValue, theme;
-    let labelConfidences = [{}];
+    export let value, theme;
+    let [labels, confidences] = [[], []];
     $: if ("confidences" in value) {
-        for (let labelConfidence in value["confidences"]) {
-            let confidence = Math.round(labelConfidence["confidence"] * 100) + "%";
-            labelConfidences.push({"label": labelConfidence.Label, "confidence": confidence})
+        for (const label_confidence of value["confidences"]) {
+            let confidence = Math.round(label_confidence["confidence"] * 100) + "%";
+            labels.push(label_confidence["label"])
+            confidences.push(confidence)
         }
     }
 </script>
 
-<!-- {#if "confidences" in value}
-{#each labels as label}
-        <div class="label" title={label}>
-          {label_confidence.label}
+{#if "confidences" in value}
+{#each labels as label, i}
+        <div class="label overflow-hidden whitespace-nowrap h-7 mb-2 overflow-ellipsis text-right" title={label}>
+          {label}
         </div>
         
         <div
-          className="confidence"
-          title={confidence}
+          class="confidence flex justify-end overflow-hidden whitespace-nowrap h-7 mb-2 px-1"
+          title={confidences[i]}
           style={{
-            minWidth: "calc(" + confidence + " - 12px)"
+            minWidth: "calc(" + confidences[i] + " - 12px)"
           }}
         >
-          {confidence}
+          {confidences[i]}
         </div>
-
-    return (
-      <div className="output_label">
-        <div className="output_class">{this.props.value["label"]}</div>
-        <div className="confidence_intervals">
-          <div className="labels" style={{ maxWidth: "120px" }}>
-            {labels}
-          </div>
-          <div className="confidences">{confidences}</div>
-        </div>
-      </div>
-    );
     {/each}
-{/if} -->
+    {:else}
+        <div class="output_label">
+            <div class="output_class_without_confidences font-bold text-2xl py-6 px-4 flex-grow flex items-center justify-center">
+            {value["label"]}
+            </div>
+        </div>
+{/if}
 
 
-<div class="output_label">
-    <div class="output_class_without_confidences font-bold text-2xl py-6 px-4 flex-grow flex items-center justify-center">
-      {value["label"]}
-    </div>
-</div>
+
 
 <style lang="postcss">
-
+.output-label[theme="default"] {
+        .label {
+            @apply text-sm h-5;
+        }
+        .confidence {
+            @apply font-mono box-border border-b-2 border-gray-300 bg-gray-200 dark:bg-gray-500 dark:border-gray-600 text-sm h-5 font-semibold rounded;
+        }
+        .confidence:first-child {
+            @apply border-yellow-600 bg-yellow-500 dark:bg-red-600 border-red-700 text-white;
+        }
+    }
 </style>
